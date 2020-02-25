2667025119b26672b57506a87f85ade64e491456
Kostiantyn Levytskyi <le.konstantos@gmail.com>
2020-02-25 15:36:21 +0200

    feat: panenco-eslint-config-react

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
   rules: {
     'react/button-has-type': 0,
-    'import/no-extraneous-dependencies': ['error', { devDependencies: ['stories/**/*.{js,jsx}', 'config/**/*.js'] }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['./stories/**/*.{js,jsx}', './config/**/*.js'] },
+    ],
   },
   settings: {
     'import/resolver': {
